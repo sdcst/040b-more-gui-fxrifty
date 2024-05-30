2f9b2bf089c0daf29304cd16ea9fb00a17e0c3f9
fxrifty <logan.stevens@gmx.com>
2024-05-30 12:40:08 -0700

    fin

---
+++ tsk1.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+Window=tk.Tk()
+Window.title=("tkk")
+Window.geometry=("2000x2000")
+buttin1=tk.Label(Window,width=20)
+buttin2=tk.Label(Window,width=15)
+buttin3=tk.Label(Window,width=15)
+buttin4=tk.Label(Window,width=15)
+buttin5=tk.Entry(Window,width=15)
+buttin6=tk.Entry(Window,width=15)
+buttin7=tk.Entry(Window,width=15)
+combo = tk.c(Window,values=["1","2","3"])
